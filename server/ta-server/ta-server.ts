6f2f29eb0fa5898c1a3d34b4f3555078e234cfbf
Leao Liu Masur <llm2@cin.ufpe.br>
2019-06-12 22:30:58 -0300

    Começo de implementação do servidor

--- server/ta-server/ta-server.ts
+++ server/ta-server/ta-server.ts
@@ -1,12 +1,14 @@
 import express = require('express');
 import bodyParser = require("body-parser");
 
-import { AtividadeEmCampo } from '../../gui/ta-gui/src/app/atividadeEmCampo';
-import {CadastroDeAtividades} from './cadastrodeatividades';
+import { AtividadeEmCampo } from '../../gui/ta-gui/src/app/atividadeEmCampo/atividadeEmCampo';
+import { CadastroDeAtividades } from './cadastrodeatividades';
+import { CadastroConsulta } from './cadastroconsultas';
+import { CadastroAlunoProfissional } from './cadastroAlunoProfissional'
 
 var app = express();
 
-var cadastro: CadastroDeAtividades = new CadastroDeAtividades();
+var cadastroAtividades: CadastroDeAtividades = new CadastroDeAtividades();
 
 var allowCrossDomain = function(req: any, res: any, next: any) {
     res.header('Access-Control-Allow-Origin', "*");
@@ -19,12 +21,12 @@ app.use(allowCrossDomain);
 app.use(bodyParser.json());
 
 app.get('/atividades', function (req, res) {
-  res.send(JSON.stringify(cadastro.getAtividades()));
+  res.send(JSON.stringify(cadastroAtividades.getAtividades()));
 })
 
 app.post('/atividade', function (req: express.Request, res: express.Response) {
   var atividade : AtividadeEmCampo = <AtividadeEmCampo> req.body; 
-  atividade = cadastro.criar(atividade);
+  atividade = cadastroAtividades.criar(atividade);
   if (atividade) {
     res.send({"success": "A atividade em campo foi cadastrado com sucesso"});
   } else {
@@ -34,7 +36,7 @@ app.post('/atividade', function (req: express.Request, res: express.Response) {
 
 app.post('/buscaAtividades', function (req: express.Request, res: express.Response) {
   var atividade : AtividadeEmCampo = <AtividadeEmCampo> req.body; 
-  var atividadesBuscadas = cadastro.busca(atividade);
+  var atividadesBuscadas = cadastroAtividades.busca(atividade);
   if (atividadesBuscadas.length>0) {
     res.send(JSON.stringify(atividadesBuscadas));
   } else {
@@ -44,22 +46,27 @@ app.post('/buscaAtividades', function (req: express.Request, res: express.Respon
 
 app.put('/atividade', function (req: express.Request, res: express.Response) {
   var atividade: AtividadeEmCampo = <AtividadeEmCampo> req.body;
-  atividade = cadastro.atualizar(atividade);
+  atividade = cadastroAtividades.atualizar(atividade);
   if (atividade) {
     res.send({"success": "A atividade em campo foi atualizado com sucesso"});
   } else {
     res.send({"failure": "A atividade em campo não pode ser atualizado"});
   }
 })
+
+
 app.delete('/atividade',function(req: express.Request, res: express.Response){
   var atividade = req.body;
-  var removido = cadastro.remover(atividade); //deveria haver um teste de remoção
+  var removido = cadastroAtividades.remover(atividade); //deveria haver um teste de remoção
   if (removido) {
     res.send({"success": "A atividade em campo foi atualizado com sucesso"});
   } else {
     res.send({"failure": "A atividade em campo não pode ser atualizado"});
   }
 });
+
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
